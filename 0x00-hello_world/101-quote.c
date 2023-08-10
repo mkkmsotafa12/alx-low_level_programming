fe4afcf9fa5fd253c3c5da2873004c25d9dad045
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-08-10 13:41:59 -0700

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - 101-quote.c
- * description - Write a c program that prints exactly and that piece of
- * art is useful\"
- *  - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
- *
- * Return: Always 0 (Success)
+ * main - A c program that prints a line to the standard error
+ * Return 1 (Success)
  */
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,15 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
+	char b;
+	int c;
+	long int d;
+	long long int c;
 	float f;
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
