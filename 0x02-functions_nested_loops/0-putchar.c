cff4d4c17953ba67a72019ef5dd1b20abc2cfcb9
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-08-15 06:57:36 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *main - Entry point
-*Discription: 'Print alphabets lowercase and uppercase'
+*Discription: 'Print _putchar to stdout'
 *Return: Always 0
 */
 int main(void)
