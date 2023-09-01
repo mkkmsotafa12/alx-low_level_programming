b97ab6d4e3a4f90fa3991def99d18c40ea205f75
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-09-01 05:47:26 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the unmber of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arg.
+ * Return: (0) when successful
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+
+printf("%d\n", argc - 1);
+
+return (0);
+
+
+}
