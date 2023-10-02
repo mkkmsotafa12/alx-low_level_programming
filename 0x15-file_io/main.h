45815e38b6dca1f5212ca14c7e8203d5030369ff
mkkmsotafa12 <kamalmostaf2@gmail.com>
2023-10-02 06:33:14 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIH_H_
+#define _MAIH_H_
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+#endif
