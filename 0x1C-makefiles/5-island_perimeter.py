241a4f558e1ff2a1a271da0d0c8954ab868dfcde
mkkmsotafa12 <mkkmsotafa12@github.com>
2024-01-24 21:23:03 +0200

    100-Makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,13 +4,17 @@
 
 def island_perimeter(grid):
     """" this function returns the perimeter of the island described in grid """
-    _ = 0
-    for _ in grid:
-        for j in _:
-            if j == 1:
-                _+= 1
-    
-    if _ == 0:
-        return 0
-    
-    return _ * 2  
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
