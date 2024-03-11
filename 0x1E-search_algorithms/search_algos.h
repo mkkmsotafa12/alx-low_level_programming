3f50bae641d0772e2ecfc3fcae164ccd734f0722
mkkmsotafa12 <mkkmsotafa12@github.com>
2024-03-12 01:03:43 +0200

    first up

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,7 @@
-#ifndef _SORTING_ALGOS_H_
-#define _SORTING_ALGOS_H_
-
-#include <stdlib.h>
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  *
@@ -12,7 +10,6 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
@@ -20,24 +17,4 @@ typedef struct listint_s
 	size_t index;
 	struct listint_s *next;
 } listint_t;
-
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- * for Holberton project
- */
-typedef struct skiplist_s
-{
-	int n;
-	size_t index;
-	struct skiplist_s *next;
-	struct skiplist_s *express;
-} skiplist_t;
-
 int linear_search(int *array, size_t size, int value);
