d9cd1ae22bf8a679f67b3bef7b0893663ca6bc17
mkkmsotafa12 <mkkmsotafa12@github.com>
2024-03-12 00:30:15 +0200

    first up

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,17 +7,18 @@
 * @value: is the value to search for
 * Return: int
 */
+
 int linear_search(int *array, size_t size, int value)
 {
-size_t i;
+	int num;
+	if (array == NULL || !array)
+		return (-1);
 
-if (array == NULL)
-return (-1);
-for (i = 0; i < size; i++)
-{
-printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-if (array[i] == value)
-return (i);
-}
-return(-1);
+	for (num = 0; num < (int)size; num++)
+	{
+		printf("Value checked array[%u] = [%d]\n", num, array[num]);
+		if (value == array[num])
+			return (num);
+	}
+	return (-1);
 }
